0ccca1cb4844e33d9b9ed422aad83db8469b6b3c
yeonsu <bestdustn@sookmyung.ac.kr>
2023-08-15 22:45:49 +0900

    spring boot project initial setting

---
+++ src/main/java/date_naeun/naeunserver/NaeunServerApplication.java
@@ -0,0 +1,13 @@
+package date_naeun.naeunserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaeunServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaeunServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/date_naeun/naeunserver/NaeunServerApplicationTests.java
@@ -0,0 +1,13 @@
+package date_naeun.naeunserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaeunServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
