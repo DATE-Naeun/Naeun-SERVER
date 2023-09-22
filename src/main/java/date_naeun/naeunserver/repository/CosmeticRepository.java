46767616851635b4dea49cd038b2f9a7ff8e10e6
yeonsu <bestdustn@sookmyung.ac.kr>
2023-09-23 01:45:01 +0900

    [FEAT] Add CosmeticRepository - 화장품 레포지토리 생성 - 나의 화장대) findAllById: 화장품 id 리스트를 받아서 화장품 엔티티가 담긴 리스트로 반환하는 메서드 구현
    related to #5

---
+++ src/main/java/date_naeun/naeunserver/repository/CosmeticRepository.java
@@ -0,0 +1,31 @@
+package date_naeun.naeunserver.repository;
+
+import date_naeun.naeunserver.domain.Cosmetic;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CosmeticRepository {
+
+    private final EntityManager em;
+
+    public Cosmetic findById(Long cosmeticId) {
+        return em.find(Cosmetic.class, cosmeticId);
+    }
+
+    public List<Cosmetic> findAllById(List<Long> cosmeticList) {
+        List<Cosmetic> cosmetics =  em.createQuery("SELECT c FROM Cosmetic c WHERE c.id IN :cosmeticList", Cosmetic.class)
+                .setParameter("cosmeticList", cosmeticList)
+                .getResultList();
+
+        if (cosmetics.isEmpty()) {
+            throw new EntityNotFoundException("해당 id의 화장품을 찾을 수 없습니다.");
+        }
+        return cosmetics;
+    }
+}
