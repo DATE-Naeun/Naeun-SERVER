df840cee62399889820c345e89fed106cd00cb8d
SeyoungYoo <yoogus12@gmail.com>
2023-10-03 01:31:30 +0900

    [FIX] Update CosmeticService - findByKeyword 추가

--- src/main/java/date_naeun/naeunserver/service/CosmeticService.java
+++ src/main/java/date_naeun/naeunserver/service/CosmeticService.java
@@ -6,10 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CosmeticService {
 
     private final CosmeticRepository cosmeticRepository;
@@ -17,5 +18,9 @@ public class CosmeticService {
     /* 화장품 id로 화장품을 가져오는 메서드 */
     public Cosmetic getCosmeticInfo(Long id) {
         return cosmeticRepository.findById(id);
+    }
+
+    public List<Cosmetic> findByKeyword(String keyword) {
+        return cosmeticRepository.findByKeyword(keyword);
     }
 }
