b85e7209a8c52a3a6ad6b14803cc42edd0e2e981
SeyoungYoo <yoogus12@gmail.com>
2023-10-12 16:50:52 +0900

    [FIX] Update SkinTypeApiController - 스킨타입이 없을 때 - 유효하지 않은 스킨타입을 입력했을 때 - 오류 처리

--- src/main/java/date_naeun/naeunserver/api/SkinTypeApiController.java
+++ src/main/java/date_naeun/naeunserver/api/SkinTypeApiController.java
@@ -1,11 +1,13 @@
 package date_naeun.naeunserver.api;
 
 import date_naeun.naeunserver.domain.SkinType;
+import date_naeun.naeunserver.dto.ResultDto;
 import date_naeun.naeunserver.dto.SkinTypeDetailDto;
 import date_naeun.naeunserver.service.SkinTypeService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,12 +20,32 @@ public class SkinTypeApiController {
     private final SkinTypeService skinType_service;
 
     @GetMapping("/api/skintype/detail")
-    public Result skinType(@RequestParam String skinType) {
-        List<SkinType> findSkinType = skinType_service.findOneSkinType(skinType);
+    public ResultDto<List<SkinTypeDetailDto>> skinType(@RequestParam String skinType) {
+        List<SkinType> findSkinType = skinType_service.findOneSkinType(skinType.toUpperCase());
+        List<SkinType> findAllSkinType = skinType_service.findAllSkinType();
+
+        if(findAllSkinType.isEmpty()) {
+            return ResultDto.of(HttpStatus.BAD_REQUEST,"스킨타입이 없습니다.", null);
+        }
+
+        // 모든 스킨타입의 이름을 추출해 목록 생성
+        List<String> allSkinTypeNames = findAllSkinType.stream()
+                .map(SkinType::getTypeName)
+                .collect(Collectors.toList());
+
+        // 입력된 스킨타입이 allSkinTypeNames 목록에 있는지 확인
+        boolean validSkinType = allSkinTypeNames.stream()
+                .anyMatch(skinType::equalsIgnoreCase);
+
+        if (!validSkinType) {
+            return ResultDto.of(HttpStatus.BAD_REQUEST, "유효하지 않은 스킨타입입니다.", null);
+        }
+
+
         List<SkinTypeDetailDto> collect = findSkinType.stream()
                 .map(SkinTypeDetailDto::new)
                 .collect(Collectors.toList());
-        return new Result(collect);
+        return ResultDto.of(HttpStatus.OK, "피부타입 상세 정보 가져오기 성공", collect);
     }
 
 
