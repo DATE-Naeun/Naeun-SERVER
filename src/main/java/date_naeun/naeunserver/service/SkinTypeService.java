c412e7f94469ea67185a83b914a5be86660b5c10
SeyoungYoo <yoogus12@gmail.com>
2023-10-13 17:29:16 +0900

    [FIX] Update SkinTypeService - user의 SkinType을 등록하는 update 메서드 추가

--- src/main/java/date_naeun/naeunserver/service/SkinTypeService.java
+++ src/main/java/date_naeun/naeunserver/service/SkinTypeService.java
@@ -1,7 +1,10 @@
 package date_naeun.naeunserver.service;
 
 import date_naeun.naeunserver.domain.SkinType;
+import date_naeun.naeunserver.domain.User;
+import date_naeun.naeunserver.dto.UpdateSkinTypeRequestDto;
 import date_naeun.naeunserver.repository.SkinTypeRepository;
+import date_naeun.naeunserver.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,7 @@ import java.util.List;
 public class SkinTypeService {
 
     private final SkinTypeRepository skinType_repository;
+    private final UserRepository user_repository;
 
     /**
      * 피부 타입 등록
@@ -44,6 +48,21 @@ public class SkinTypeService {
      */
     public List<SkinType> findOneSkinType(String typeName) {
         return skinType_repository.findOneSkinType(typeName);
+    }
+
+    public SkinType findSkinType(String typeName) {
+        return skinType_repository.findSkinType(typeName);
+    }
+
+    /**
+     * user의 SkinType 등록
+     */
+    @Transactional
+    public void update(Long userId, UpdateSkinTypeRequestDto updateSkinTypeRequestDto) {
+        User user = user_repository.findOne(userId);
+        user.setSkinType(findSkinType(updateSkinTypeRequestDto.getSkinType()));
+        user.setTrouble(updateSkinTypeRequestDto.getTrouble());
+        user.setWhitening(updateSkinTypeRequestDto.getWhitening());
     }
 
 }
