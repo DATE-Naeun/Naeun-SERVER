52758b22f3b4fbb0039d5f5f06b23cfe20c74d9f
SeyoungYoo <yoogus12@gmail.com>
2023-10-20 02:54:47 +0900

    [FIX] Update ApiErrorStatus - Ingredient와 관련된 예외상황들에 관한 상태 추가   - 검색 결과가 없습니다.   - 성분 list가 없습니다.   - 가 정수가 아닙니다.   - 가 중복되었습니다.   - 가 존재하지 않습니다.   - 선호 성분이 등록되지 않았습니다.   - 기피 성분이 등록되지 않았습니다.

--- src/main/java/date_naeun/naeunserver/exception/ApiErrorStatus.java
+++ src/main/java/date_naeun/naeunserver/exception/ApiErrorStatus.java
@@ -19,7 +19,22 @@ public enum ApiErrorStatus {
     LIST_NOT_EXIST(HttpStatusCode.BAD_REQUEST, "화장품 list가 없습니다."),
     NOT_INTEGER(HttpStatusCode.BAD_REQUEST, "가 정수가 아닙니다."),
     DUPLICATED_ID(HttpStatusCode.BAD_REQUEST, "가 중복되었습니다."),
-    NOT_EXIST(HttpStatusCode.BAD_REQUEST, "가 존재하지 않습니다.");
+    NOT_EXIST(HttpStatusCode.BAD_REQUEST, "가 존재하지 않습니다."),
+
+
+    /**
+     * Ingredient Api 관련 에러 코드
+     */
+    /* searchIngredient (검색) */
+    INGR_NOT_RESULT(HttpStatusCode.BAD_REQUEST, "검색 결과가 없습니다."),
+
+    /* UserIngredient 추가 및 삭제 */
+    INGR_LIST_NOT_EXIST(HttpStatusCode.BAD_REQUEST, "성분 list가 없습니다."),
+    INGR_NOT_INTEGER(HttpStatusCode.BAD_REQUEST, "가 정수가 아닙니다."),
+    INGR_DUPLICATED_ID(HttpStatusCode.BAD_REQUEST, "가 중복되었습니다."),
+    INGR_ID_NOT_EXIST(HttpStatusCode.BAD_REQUEST, "가 존재하지 않습니다."),
+    INGR_PREFER_NOT_EXIST(HttpStatusCode.BAD_REQUEST, "선호 성분이 등록되지 않았습니다."),
+    INGR_DISLIKE_NOT_EXIST(HttpStatusCode.BAD_REQUEST, "기피 성분이 등록되지 않았습니다.");
 
     private final HttpStatusCode code;
     private final String msg;
