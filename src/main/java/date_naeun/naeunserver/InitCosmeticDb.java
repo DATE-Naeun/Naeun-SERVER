0b986ead670ba0cfbc7e5e1fb396e39e086e24d9
SeyoungYoo <yoogus12@gmail.com>
2023-10-24 05:05:42 +0900

    [FEAT] Add InitCosmeticDb - 스프레드 시트 추가 - 화장품 데이터 DB 등록 - 기존 InitCosmeticSampleDb의 Init 부분 주석 처리

---
+++ src/main/java/date_naeun/naeunserver/InitCosmeticDb.java
@@ -0,0 +1,83 @@
+package date_naeun.naeunserver;
+
+import date_naeun.naeunserver.domain.Cosmetic;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.apache.poi.ss.usermodel.*;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Iterator;
+
+@Component
+@RequiredArgsConstructor
+public class InitCosmeticDb {
+
+    private final InitCosmeticDbService initCosmeticDbService;
+    @PostConstruct
+    public void init() { initCosmeticDbService.initDb(); }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitCosmeticDbService {
+
+        private final EntityManager em;
+
+        public void initDb() {
+            try (FileInputStream fis = new FileInputStream("src/main/resources/Cosign_Dataset_v1.xlsx");
+                 Workbook workbook = new XSSFWorkbook(fis)) {
+
+                Sheet sheet = workbook.getSheetAt(1);
+
+                Iterator<Row> rowIterator = sheet.iterator();
+                while (rowIterator.hasNext()) {
+                    Row row = rowIterator.next();
+                    if (row.getRowNum() == 0) {
+                        continue;
+                    }
+
+                    String name = row.getCell(0).getStringCellValue();
+                    String brand = row.getCell(1).getStringCellValue();
+                    String image = row.getCell(2).getStringCellValue();
+                    String category = row.getCell(3).getStringCellValue();
+
+                    Cell priceCell = row.getCell(4);
+                    int priceInt = 0;
+
+                    if (priceCell != null) {
+                        if (priceCell.getCellType() == CellType.NUMERIC) {
+                            double priceDouble = priceCell.getNumericCellValue();
+                            priceInt = (int) priceDouble;
+                        } else {
+                            priceInt = 0;
+                        }
+                    }
+
+                    DecimalFormat decimalFormat = new DecimalFormat("###,###");
+                    String price = decimalFormat.format(priceInt);
+
+                    Cosmetic cosmetic = Cosmetic.builder()
+                            .name(name)
+                            .brand(brand)
+                            .image(image)
+                            .category(category)
+                            .price(price)
+                            .build();
+
+                    em.persist(cosmetic);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
